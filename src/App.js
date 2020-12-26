3211923a0d62dd216ae23221da70d1c26755e23d
hani hinnawi <hani.hinnawi@outlook.com>
2020-12-26 11:04:59 +0200

    develop

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import axios from "./axios";
+import Body from "./Body";
+import Login from "./Login";
+import Navbar from "./Navbar";
+import requests from "./requests";
+import Row from "./Row";
+import { useStateValue } from "./StateProvider";
+import Trailer from "./Trailer";
 function App() {
+  const [{ user, token }, dispatch] = useStateValue();
+  const [movieState, setMovie] = useState([]);
+  useEffect(() => {
+    console.log("inside useEffect");
+    async function fetchData() {
+      const request = await axios.get(requests.getAllMovies);
+      setMovie(request.data);
+      console.log(movieState);
+    }
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path="/video">
+            <Navbar />
+            <Trailer/>
+          </Route>
+          <Route path="/">
+            <Navbar />
+            {/* navBar */}
+            <Body />
+
+            <Row
+              title="Top Picks"
+              desc="Sign in to rate and Watchlist for personalized Top picks recommendations"
+              movieState={movieState}
+            />
+            <Row
+              title="Fan favorites"
+              desc="This week's top TV and movies"
+              movieState={movieState.filter(
+                (featured) => featured.IsFeatured === true
+              )}
+            />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { StateProvier } from "./StateProvider";
+import reducer, { initialState } from "./reducer";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StateProvier initialState={initialState} reducer={reducer}>
+      {reducer}
+      <App />
+    </StateProvier>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
